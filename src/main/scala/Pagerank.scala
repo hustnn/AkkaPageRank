e6205140e677bfa2208490bd1f5e614821fe2004
zhaojie <zhaojie@PL02-3.ntu.edu.sg>
2016-04-14 15:31:17 +0800

    initial commit from local

---
+++ src/main/scala/Pagerank.scala
@@ -0,0 +1,17 @@
+/**
+  * Created by zhaojie on 4/13/16.
+  */
+
+import akka.actor
+import scala.concurrent.{Future,Await}
+import akka.pattern.ask
+import akka.util._
+import scala.concurrent.duration._
+
+object Pagerank {
+
+  def main (args: Array[String]): Unit = {
+
+  }
+
+}
